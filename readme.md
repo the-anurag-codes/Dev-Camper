95e64c9be49031344353271a01881d99d1ee7848
Star Anurag <anuragchauhan48177@gmail.com>
2024-02-06 23:21:53 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Star DevCamper API
+
+> Backend API for DevCamper application, which is a bootcamp directory website
+
+## Usage
+
+Rename "config/config.env.env" to "config/config.env" and updates the values/settings to your own
+
+## Install Dependencies
+```
+npm install
+```
+
+## Run app
+```
+# Run in dev mode
+npm run dev
+
+# Run in prod mode
+npm start
+```
+
+- Version: 1.0.0
+- License: Star
---
+++ server.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB(); 
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev login middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// File Uploading
+app.use(fileupload());
+
+// Sanatize Data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attaks
+app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 min,
+    max: 100
+});
+
+app.use(limiter);
+
+// Prevent http paramss pollution
+app.use(hpp());
+
+// Enabled CORS
+app.use(cors());
+
+// Set staic folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & stop process
+    server.close(() => process.exit(1));
+})
